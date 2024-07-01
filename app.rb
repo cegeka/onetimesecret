736ca26d0a0279995ce3c3f6f13edd56e59309d5
Bogdan Stan <32199254+stanbog@users.noreply.github.com>
2024-07-01 12:39:43 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ require "prometheus/middleware/collector"
 require "prometheus/middleware/exporter"
 
 use Prometheus::Middleware::Collector
-#use Prometheus::Middleware::Exporter
+use Prometheus::Middleware::Exporter
 
 include ERB::Util
 
